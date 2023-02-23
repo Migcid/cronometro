38c4e090df510131e18b660f5ae7de89323c2959
Miguel Cid Y <miguelcidyevenes@gmail.com>
2023-02-23 00:01:39 -0300

    Creando un cronometro

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const numeros = document.getElementById('numeros');
+const botonIniciar = document.getElementById('iniciar');
+
+let stopWatchInterval;
+let runningTime = 0;
+
+const iniciarPausar = () => {
+    
+}
